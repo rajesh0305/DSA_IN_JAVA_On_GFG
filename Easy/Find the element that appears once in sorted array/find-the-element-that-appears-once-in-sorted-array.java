5a6bc6950c4e7d3f298c64b58e6c247525e8c333
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-05-17 17:17:58 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0;i < n;i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            System.out.println(sol.findOnce(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution 
+{
+    int findOnce(int arr[], int n)
+    {
+        // Complete this function
+        int left=0;
+        int right=n-1;
+        
+        if(n==1){
+            return arr[0];
+        }
+        
+        while(left<=right){
+            
+            int mid=(left+right)/2;
+            //left
+            if(mid<n-1 && mid%2==0 && arr[mid]==arr[mid+1]){
+                left=mid+1;
+            }
+            else if(mid>0 && mid%2!=0 && arr[mid]==arr[mid-1]){
+                left=mid+1;
+            }
+            //right
+            else if(mid>0 && mid%2==0 && arr[mid]==arr[mid-1]){
+                right=mid-1;
+            }
+            else if(mid<n-1 && mid%2!=0 && arr[mid]==arr[mid+1]) {
+                right=mid-1;
+            }
+            else{
+                return arr[mid];
+            }
+            
+        }
+        return -1;
+            
+    }
+}
