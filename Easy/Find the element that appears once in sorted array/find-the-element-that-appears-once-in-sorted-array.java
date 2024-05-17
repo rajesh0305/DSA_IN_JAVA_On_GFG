4afeabd650a9e3cc312892c10c2ae18e4cd9e55c
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-05-17 17:33:50 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.java
+++ Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.java
@@ -34,36 +34,42 @@ class Solution
     int findOnce(int arr[], int n)
     {
         // Complete this function
-        int left=0;
-        int right=n-1;
+      //  Approach 1--->O(logN)
+      
+        // int left=0;
+        // int right=n-1;
         
-        if(n==1){
-            return arr[0];
-        }
+        // if(n==1){
+        //     return arr[0];
+        // }
         
-        while(left<=right){
+        // while(left<=right){
             
-            int mid=(left+right)/2;
-            //left
-            if(mid<n-1 && mid%2==0 && arr[mid]==arr[mid+1]){
-                left=mid+1;
-            }
-            else if(mid>0 && mid%2!=0 && arr[mid]==arr[mid-1]){
-                left=mid+1;
-            }
-            //right
-            else if(mid>0 && mid%2==0 && arr[mid]==arr[mid-1]){
-                right=mid-1;
-            }
-            else if(mid<n-1 && mid%2!=0 && arr[mid]==arr[mid+1]) {
-                right=mid-1;
-            }
-            else{
-                return arr[mid];
-            }
+        //     int mid=(left+right)/2;
+        //     //left
+        //     if(mid<n-1 && mid%2==0 && arr[mid]==arr[mid+1]){
+        //         left=mid+1;
+        //     }
+        //     else if(mid>0 && mid%2!=0 && arr[mid]==arr[mid-1]){
+        //         left=mid+1;
+        //     }
+        //     //right
+        //     else if(mid>0 && mid%2==0 && arr[mid]==arr[mid-1]){
+        //         right=mid-1;
+        //     }
+        //     else if(mid<n-1 && mid%2!=0 && arr[mid]==arr[mid+1]) {
+        //         right=mid-1;
+        //     }
+        //     else{
+        //         return arr[mid];
+        //     }
             
-        }
-        return -1;
-            
+        // }
+        // return -1;
+        int ele = arr[0];
+        for(int i =1;i<arr.length;i++){
+            ele = ele^arr[i];
+        }    
+        return ele;
     }
 }
