ef06df44bd0ed40d24524d45e42050d04879214b
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-07-01 07:40:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.java
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}
+
+class DLinkedList
+{
+	
+	Node newNode(Node head, int data)
+	{
+		Node n = new Node(data);
+		if(head == null)
+		{
+		    head = n;
+		    return head;
+		}
+		head.next = n;
+		n.prev = head;
+		head = n;
+		return head;
+	}
+	
+	
+	void printList(Node node)
+	{
+		Node temp = node;
+		while(temp.next != null)
+		{
+			temp = temp.next;
+		}
+		
+		while(temp.prev != null)
+		{
+			temp = temp.prev;
+		}
+		
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		DLinkedList DLL = new DLinkedList();
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			Node temp;
+			Node head = null;
+			Node root = null;
+			int n = sc.nextInt();
+			for(int i=0;i<n;i++)
+			{
+				int x = sc.nextInt();
+				head = DLL.newNode(head,x);
+			    if(root == null)    root = head;
+			}
+			head = root;
+			int pos = sc.nextInt();
+			int data = sc.nextInt();
+			
+		    GfG g = new GfG();
+		    g.addNode(head,pos,data);
+			
+			DLL.printList(head);
+			while(head.next != null)
+			{
+				temp = head;
+				head = head.next;
+			}
+		    t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Structure of Doubly Linked List
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}*/
+
+class GfG
+{
+    //Function to insert a new node at given position in doubly linked list.
+    void addNode(Node head_ref, int pos, int data)
+	{
+		// Your code here
+		
+// 		int i =0;
+// 		Node temp = head_ref;
+// 		Node newnode  = new Node (data);
+		
+// 	    while(i!=pos){
+// 	        temp= temp.next;
+// 	        i = i+1;
+// 	    }
+// 	    temp.next = newnode;
+// 	    newnode.prev= temp;
+// 	   // temp = temp.next;
+// 	   // newnode.next = temp;
+// 	   // temp.prev= newnode;
+// 	      if(newnode.next) 
+//         newnode.next.prev=newnode;
+
+
+
+Node newnode = new Node(data);
+if(head_ref == null)
+head_ref = newnode;
+ 
+ 
+Node curr = head_ref;
+int i = 1;
+while(curr != null && i<=pos)
+{   i++;
+    curr = curr.next;
+    
+}
+if(curr==null){
+    return;
+}
+//edge case
+if(curr.next == null)
+{
+    curr.next = newnode;
+    newnode.prev = curr;
+}
+else
+{
+Node ptr = curr.next;
+    
+    curr.next = newnode;
+    ptr.prev = newnode;
+    newnode.next = ptr;
+    newnode.prev = curr;   
+}
+	}
+}
