665ddefe935c934a1449d28dcc66ea7b2856fa48
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-07-08 13:02:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Right Leaf Nodes/sum-of-right-leaf-nodes.java
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+
+        while(t > 0){
+            String s = br.readLine();
+	    	Node root = buildTree(s);
+	    	
+            Solution ob = new Solution();
+    		System.out.println(ob.rightLeafSum(root));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    int sum = 0;
+       public static int helper(Node root, boolean flag){
+        if(root==null){
+            return 0;
+        }
+        if(root.left==null && root.right==null && flag){
+            return root.data;
+        }
+        int s1 =helper(root.left,false);
+        int s2 = helper(root.right,true);
+        return (s1+s2);
+        
+       } 
+    int rightLeafSum(Node root)
+    {
+        // code here
+    //   boolean flag = false;
+    //     int sum = helper(root,flag);
+    //     return sum;
+           if(root==null)
+         return 0;
+         
+         if(root.right!=null && root.right.left==null && root.right.right==null)
+            sum+=root.right.data;
+           
+           rightLeafSum(root.left);
+           rightLeafSum(root.right);
+           
+           return sum;
+    }
+}
