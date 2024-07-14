0ca82fa9cb585e0ad39312bb7a260eaefbd413be
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-07-14 10:42:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Lucky Numbers/lucky-numbers.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(reader.readLine());//testcases
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(reader.readLine());//input n
+            //printing "1" if isLucky() returns true else "0"
+            Solution s = new Solution();
+            System.out.println(s.isLucky(n)? "1" : "0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution
+{
+    public static boolean fun(int pos,int c){
+         if(pos<c){
+             return true;
+         }
+         if(pos%c==0){
+             return false;
+         }
+         pos = pos-(pos/c);
+         c = c+1;
+         return fun(pos,c);
+    }
+    // Complete the function
+    // n: Input n
+    // Return True if the given number is a lucky number else return False
+    public static boolean isLucky(int n)
+    {
+        // Your code here
+        return fun(n,2);
+    }
+}
