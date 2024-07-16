1c4c044d3be7f73e6d73dd41964ece6fcd31f587
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-07-16 16:28:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.java
@@ -0,0 +1,172 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Tree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                GfG g = new GfG();
+                if(g.isSymmetric(root) == true)
+                    System.out.println("True");
+                else
+                    System.out.println("False");
+        	
+                t--;
+            
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+/*
+class of the node of the tree is as
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+*/
+// complete this function
+// return true/false if the is Symmetric or not
+class GfG
+{
+    public static boolean isSymmetric(Node t1,Node t2){
+       if(t1==null && t2!=null){
+           return false ;
+           
+       }
+       if(t1!=null && t2==null){
+           return false ;
+       }
+       if(t1==null && t2==null){
+           return true;
+       }
+        return ((t1.data==t2.data)&&isSymmetric(t1.left,t2.right)&&isSymmetric(t2.left,t1.right));
+    }
+// Maintain two Node pointers in the tree.
+// If the first moves right, the other moves left.
+// If the first moves left, the other moves right.
+
+// And, for every pair of node we get through the above traversal, we check the following:
+// if node1 is NULL and node2 is not NULL: The tree is not symmetric, return False.
+// If node1 is not NULL and node2 is NULL: The tree is not symmetric, return False.
+// If both node1 and node2 are NULL: Return True.
+// If both node1 and node2 are not NULL: 
+// Return ((node->data == node2->data) && (isSymm (node1->left, node2->right) 
+// && (isSymm (node1->right, node2->left)
+    // return true/false denoting whether the tree is Symmetric or not
+    public static boolean isSymmetric(Node root)
+    {
+        // add your code here;
+    //   return isSymmetric(root.left,root.right);
+             if (root == null) {
+            return true;
+        }
+       return symetric(root.left,root.right);
+    }
+   static boolean  symetric(Node left,Node right){
+        if(left==null&&right==null)return true;
+        if((left!=null&&right==null)||(left==null&&right!=null))return false;
+        return left.data==right.data && symetric(left.left,right.right)&&symetric(left.right,right.left);
+    }
+}
