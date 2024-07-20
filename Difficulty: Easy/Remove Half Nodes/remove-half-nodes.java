a26f3f091cd4cf03fbc72c6ca433d34ac99403b5
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-07-20 09:44:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Half Nodes/remove-half-nodes.java
+++ Difficulty: Easy/Remove Half Nodes/remove-half-nodes.java
@@ -1,116 +1,111 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.LinkedList; 
-import java.util.Queue; 
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Node{
+class Node {
     int data;
     Node left;
     Node right;
-    Node(int data){
+
+    Node(int data) {
         this.data = data;
-        left=null;
-        right=null;
+        left = null;
+        right = null;
     }
 }
 
 class GfG {
-    
-    static Node buildTree(String str){
-        
-        if(str.length()==0 || str.charAt(0)=='N'){
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
             return null;
         }
-        
+
         String ip[] = str.split(" ");
         // Create the root of the tree
         Node root = new Node(Integer.parseInt(ip[0]));
         // Push the root to the queue
-        
-        Queue<Node> queue = new LinkedList<>(); 
-        
+
+        Queue<Node> queue = new LinkedList<>();
+
         queue.add(root);
         // Starting from the second element
-        
+
         int i = 1;
-        while(queue.size()>0 && i < ip.length) {
-            
+        while (queue.size() > 0 && i < ip.length) {
+
             // Get and remove the front of the queue
             Node currNode = queue.peek();
             queue.remove();
-                
+
             // Get the current node's value from the string
             String currVal = ip[i];
-                
+
             // If the left child is not null
-            if(!currVal.equals("N")) {
-                    
+            if (!currVal.equals("N")) {
+
                 // Create the left child for the current node
                 currNode.left = new Node(Integer.parseInt(currVal));
                 // Push it to the queue
                 queue.add(currNode.left);
             }
-                
+
             // For the right child
             i++;
-            if(i >= ip.length)
-                break;
-                
+            if (i >= ip.length) break;
+
             currVal = ip[i];
-                
+
             // If the right child is not null
-            if(!currVal.equals("N")) {
-                    
+            if (!currVal.equals("N")) {
+
                 // Create the right child for the current node
                 currNode.right = new Node(Integer.parseInt(currVal));
-                    
+
                 // Push it to the queue
                 queue.add(currNode.right);
             }
             i++;
         }
-        
+
         return root;
     }
-    static void printInorder(Node root)
-    {
-        if(root == null)
-            return;
-            
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
         printInorder(root.left);
-        System.out.print(root.data+" ");
-        
+        System.out.print(root.data + " ");
+
         printInorder(root.right);
     }
-    
-	public static void main (String[] args) throws IOException{
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        
-	        int t=Integer.parseInt(br.readLine());
-    
-	        while(t > 0){
-	            String s = br.readLine();
-    	    	Node root = buildTree(s);
-        	    Tree g = new Tree();
-			    Node fresh = g.RemoveHalfNodes(root);
-			    printInorder(fresh);
-			    System.out.println();
-                t--;
-            
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            Node fresh = g.RemoveHalfNodes(root);
+            printInorder(fresh);
+            System.out.println();
+            t--;
         }
     }
-  
 }
-
 
 // } Driver Code Ends
 
 
-//User function Template for Java
-
+// User function Template for Java
 
 /*
 
@@ -125,28 +120,35 @@ class Node{
 
 */
 
-class Tree
-{
-    // Return the root of the modified tree after removing all the half nodes.
-    public static Node RemoveHalfNodes(Node root)
-    {
-        // Code Here
-         if(root == null) return root;
-        
-        Node leftNode = RemoveHalfNodes(root.left);
-        Node rightNode = RemoveHalfNodes(root.right);
-        
-        if(leftNode == null && rightNode == null) {
+class Solution {
+    public static Node remove(Node root){
+        if(root.left==null && root.right==null){
             return root;
-        } else if(leftNode == null) {
-            root.right = rightNode;
-            return rightNode;
-        } else if(rightNode == null) {
-            root.left = leftNode;
-            return leftNode;
-        } else {
-            root.left = leftNode;
-            root.right = rightNode;
-            return root;}
+        }
+        Node r = null;
+        Node l = null;
+        if(root.left!=null){
+            l = remove(root.left);
+            
+        }
+        if(root.right!=null){
+            r = remove(root.right);
+            
+        }
+        root.left = l;
+        root.right = r;
+        if(l==null){
+            return r;
+        }
+        if(r ==null){
+            return l;
+        
+    }
+    return root;
+    }
+    // Return the root of the modified tree after removing all the half nodes.
+    public static Node RemoveHalfNodes(Node root) {
+        // Code Here
+        return remove(root);
     }
 }
