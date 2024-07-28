819b329b578e014a0c90aeb540d73429a0088296
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-07-28 09:47:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Number is sparse or not/number-is-sparse-or-not.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to check if the number is sparse or not.
+    public static boolean isSparse(int n)
+    {
+        // Your code here
+          int pos1=0,pos2=0;
+        //boolean flag=false;
+        while(n>0)
+        {
+            pos1=n%2;
+            pos2=(n/2)%2;
+            if((pos1 == 1) && (pos2 == 1))
+                return false;
+            n=n/2;
+        }
+        return true;
+    }
+    
+}
+
+
+//{ Driver Code Starts.
+
+class Main {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//testcases
+		int t = sc.nextInt();
+		
+		while(t-->0){
+		    
+		    		     
+            //initializing n
+		    int n = sc.nextInt();
+		    
+		    Solution obj = new Solution();
+		    
+		    //printing 1 if isSparse returns true
+            //else 0
+		    if(obj.isSparse(n)){
+		        System.out.println("1");
+		    } else {
+                System.out.println("0");
+		    }
+		}
+	}
+}
+// } Driver Code Ends
