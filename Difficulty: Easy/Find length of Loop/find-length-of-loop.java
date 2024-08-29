f1ed84ba92fbd07d44dbff659e2c798ba41735c2
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-08-29 12:50:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find length of Loop/find-length-of-loop.java
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.java
@@ -1,58 +1,56 @@
 //{ Driver Code Starts
-// driver code
-
-import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class Node
-{
+class Node {
     int data;
     Node next;
-    
-    Node(int x)
-    {
+
+    Node(int x) {
         data = x;
         next = null;
     }
 }
 
-class GFG
-{
-    public static void makeLoop(Node head, Node tail, int x){
+public class LinkedList {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void makeLoop(Node head, Node tail, int x) {
         if (x == 0) return;
-        
+
         Node curr = head;
-        for(int i=1; i<x; i++)
-            curr = curr.next;
-        
+        for (int i = 1; i < x; i++) curr = curr.next;
+
         tail.next = curr;
     }
-    
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t--> 0)
-        {
-            int n = sc.nextInt();
-            
-            int num = sc.nextInt();
-            Node head = new Node(num);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
             Node tail = head;
-            
-            for(int i=0; i<n-1; i++)
-            {
-                num = sc.nextInt();
-                tail.next = new Node(num);
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
                 tail = tail.next;
             }
-            
-            int pos = sc.nextInt();
-            makeLoop(head, tail, pos);
-            
-            Solution x = new Solution();
-            System.out.println( x.countNodesinLoop(head) );
+            makeLoop(head, tail, k);
+
+            Solution ob = new Solution();
+            System.out.println(ob.countNodesinLoop(head));
         }
     }
 }
@@ -71,27 +69,28 @@ class Node
 
 */
 
-//Function should return the length of the loop in LL.
+// Function should return the length of the loop in LL.
 
-class Solution
-{
-    //Function to find the length of a loop in the linked list.
-    static int countNodesinLoop(Node head)
-    {
-        //Add your code here.
-        HashMap<Node,Integer>map =new HashMap<Node ,Integer>();
-        int count = 1;
-        Node temp = head;
-        while(temp!=null){
-            if(map.containsKey(temp)){
-                int val = map.get(temp);
-                return (count-val);
-            }else{
-                map.put(temp,count);
-                count++;
-                temp= temp.next;
+class Solution {
+    // Function to find the length of a loop in the linked list.
+    public int countNodesinLoop(Node head) {
+        // Add your code here.
+        Node f =head;
+        Node s = head;
+        int count = 0;
+        while(f!=null && f.next!=null){
+            s = s.next;
+            f = f.next.next;
+            if(f==s){
+                f = f.next;
+                count = count +1;
+                while(f!=s){
+                    f = f.next;
+                    count = count+1;
+                }
+                break;
             }
         }
-        return 0;
+        return count;
     }
 }
