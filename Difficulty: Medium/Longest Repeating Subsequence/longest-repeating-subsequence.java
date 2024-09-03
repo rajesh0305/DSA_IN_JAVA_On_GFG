2c630e321279f9ef0d79dd8b2bccb1602925c664
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-09-03 16:28:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.LongestRepeatingSubsequence(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int LongestRepeatingSubsequence(String str)
+    {
+        // code here
+        String str1 = str;
+        int n = str.length();
+        int m = str1.length();
+        int dp[][] = new int [n+1][m+1];
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(str1.charAt(i-1)==str.charAt(j-1) && i!=j){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
