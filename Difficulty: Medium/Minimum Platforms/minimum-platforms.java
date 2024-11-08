0e17634b6644e06b097984739ab7ce04995dd403
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-11-08 10:08:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.java
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -1,72 +1,62 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0)
-        {
+
+        while (t-- > 0) {
             String str[] = read.readLine().trim().split(" ");
             int n = Integer.parseInt(str[0]);
-            
+
             int arr[] = new int[n];
             int dep[] = new int[n];
-            
+
             str = read.readLine().trim().split(" ");
-            for(int i = 0; i < n; i++)
-              arr[i] = Integer.parseInt(str[i]);
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(str[i]);
             str = read.readLine().trim().split(" ");
-            for(int i = 0; i < n; i++)
-                dep[i] = Integer.parseInt(str[i]);
-                
-            System.out.println(new Solution().findPlatform(arr, dep, n));
+            for (int i = 0; i < n; i++) dep[i] = Integer.parseInt(str[i]);
+
+            System.out.println(new Solution().findPlatform(arr, dep));
+
+            System.out.println("~");
         }
     }
-    
-    
 }
-
-
 
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution
-{
-    //Function to find the minimum number of platforms required at the
-    //railway station such that no train waits.
-    static int findPlatform(int arr[], int dep[], int n)
-    {
+class Solution {
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[]) {
         // add your code here
         Arrays.sort(arr);
         Arrays.sort(dep);
-        int i =0;
-        int j = 0;
-        int maxPlateform= 0;
-        int Plateform = 0;
-        while(i<n && j<n){
-            if(arr[i]<=dep[j]){
-                Plateform = Plateform+1;
-                i++;
+        int ptr1 = 0;
+        int ptr2 = 0;
+        int count = 0;
+        int maxcount = 0;
+        int n = arr.length;
+        
+        while(ptr1<n){
+            if(arr[ptr1]<=dep[ptr2]){
+                count++;
+                ptr1 = ptr1+1;
             }else{
-                Plateform= Plateform-1;
-                j++;
+                count = count-1;
+                ptr2 =ptr2+1;
             }
-            maxPlateform = Math.max(maxPlateform,Plateform);
-            
+            maxcount = Math.max(maxcount,count);
         }
-        return maxPlateform;
+        return maxcount;
     }
-    
 }
-
